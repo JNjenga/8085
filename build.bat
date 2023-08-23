1adc9459fd4823c32089c5232eb65a0043fb1902
JNjenga <me@jnjenga.com>
2023-08-23 19:19:47 +0300

    Add imgui deps

--- build.bat
+++ build.bat
@@ -1,22 +1,22 @@
 
 @ECHO OFF
 
-REM SET LIB_DIRS=/LIBPATH:..\deps\glfw3\libvc2019
+SET LIB_DIRS=/LIBPATH:..\thirdparty\glfw\lib-vc2010-64
 
-REM SET LIBS=kernel32 GDI32.lib shll32.lib user32.lib opengl32.lib glfw3_mt.lib glew32s.lib
+SET LIBS= glfw3.lib kernel32.lib opengl32.lib gdi32.lib shell32.lib
 
-REM SET INCLUDE DIRS=/I ..\deps\glfw3\include
+SET INCLUDE_DIRS=/I..\thirdparty\glfw\include /I..\thirdparty\imgui\backends /I..\thirdparty\imgui
 
-SET SRC_FILES=..\src\*.cpp
-SET EXECUTABLE=lib8085test.exe
+SET SRC_FILES=..\src\*.cpp ..\thirdparty\imgui\backends\imgui_impl_glfw.cpp ..\thirdparty\imgui\backends\imgui_impl_opengl3.cpp ..\thirdparty\imgui\imgui*.cpp 
+SET MAIN_FILE=..\src\main.cpp
 
-SET CFLAGS=/EHsc /Zi /nologo /Fe"lib8085test"
+SET CFLAGS=/EHsc /MD /Zi /nologo /Fe"lib8085test" 
 
 pushd .
 mkdir build
 cd build
 
-cl %SRC_FILES% %CFLAGS%
+cl %SRC_FILES% %MAIN_FILE% %INCLUDE_DIRS% %CFLAGS% /link %LIB_DIRS% %LIBS%
 
 if ERRORLEVEL 1 GOTO EXIT
 call lib8085test.exe
